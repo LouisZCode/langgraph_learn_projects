09d0a60ea9a5d63f4562be82a1a76573914cb4cb
Luis Zermeno <lgzg90@hotmail.com>
2026-03-13 14:00:16 +0100

    feature: first graph created and working

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from langgraph-practice!")
+from tkinter import END
+from unittest import result
+from langgraph import graph
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+class HelloWorldState(TypedDict):
+    greeting : str
+
+def hello_world_node(state: HelloWorldState):
+    state["greeting"] = "Hello World" + state["greeting"]
+    return state
 
 
-if __name__ == "__main__":
-    main()
+builder = StateGraph(HelloWorldState)
+builder.add_node("greet", hello_world_node)
+builder.add_edge(START, "greet")
+builder.add_edge("greet" , END)
+
+graph = builder.compile()
+result = graph.invoke({"greeting" : " from LangGraph"})
+
+print(result)
