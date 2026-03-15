9c7d4f717f8ce12a6bc5bfc84647b326a370073d
Luis Zermeno <lgzg90@hotmail.com>
2026-03-15 17:29:44 +0100

    exercise: add f node logic for multiple satate schema exercvise

--- 01_multiple_state_schemas/Lesson3b.py
+++ 01_multiple_state_schemas/Lesson3b.py
@@ -16,4 +16,21 @@ class PrivateState(TypedDict):
     private_message:str
 
 def add_world(state:InputState) -> OverallState:
-    pass
+    partial_answer = state['user_input'] + "World"
+    print(f"Node 1 - add_world: Transformed '{state['user_input']}' to '{partial_answer}'")
+ 
+    return {"partial_message" : partial_answer, "user_input": state["user_input"], "messages_output":""}
+
+def add_exclamation(state:OverallState) -> OverallState:
+    private_message = state["partial_message"] + "!"
+    print(f"Node 2 - add_exclamation: Transformed '{state['partial_message']}' to '{private_message}'")
+
+    return {"private_message" : private_message}
+
+def finalize_message(state:PrivateState) -> OutputState:
+    message_output = state["private_message"]
+    print(f"Node 3 - finalize_message: Finalized message to '{message_output}'")
+
+    return{"messages_output":message_output}
+
+builder = StateGraph(OverallState, input=InputState, output=OutputState)
