3628668d43ec5bb0f679b997df048c4a149e37e9
Luis Zermeno <lgzg90@hotmail.com>
2026-03-30 13:01:14 +0200

    feature: changed base og lont term lesson to postgres not simple InMemoryStore

--- 05_memory/long-term.py
+++ 05_memory/long-term.py
@@ -1,32 +1,38 @@
-from langgraph.checkpoint.memory import InMemoryStore
+from dotenv import load_dotenv
+from langgraph.store.postgres import PostgresStore
 from langgraph import graph
 from langgraph.graph import StateGraph, MessagesState, START, END
+import os
 
 import uuid
 
-in_memory_store = InMemoryStore()
 
-def store_user_info(state : MessagesState, config, *, store=in_memory_store):
+load_dotenv()
+database = os.getenv("DATABASE_URL")
+
+pg_store = PostgresStore.from_conn_string(database)
+
+def store_user_info(state : MessagesState, config, *, store=pg_store):
     user_id = config["configurable"]["user_id"]
-    Namespace = (user_id, "memories")
+    namespace = (user_id, "memories")
 
     memory_id = str(uuid.uuid4())
-    memory = ("user_name" : state["user_name"])
+    memory = {"user_name" : state["user_name"]}
 
-    store.put(namespace.memory_id, memory)
+    store.put(namespace, memory_id, memory)
 
     return {"messages" : ["User information saved"]}
 
-def retrieve_user_information(state: MessagesState, config, *, store=in_memory_store):
+def retrieve_user_information(state: MessagesState, config, *, store=pg_store):
     user_id = config["configurable"]["user_id"]
     namespace = (user_id, "memories")
 
     memories = store.search(namespace)
 
     if memories:
-        info = f"Hello {memories[-1].value["user_name"]}, welcome back!"
+        info = f"Hello {memories[-1].value['user_name']}, welcome back!"
 
     else:
         info = "I dont have enough information about you"
 
-    return {"messages" , [info]}
+    return {"messages" : [info]}
